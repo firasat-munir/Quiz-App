a40fb9dc2ee58d8d3be3efd5c1ef12d9d57b1d66
Firasat Munir <zapp3r1997@gmail.com>
2022-09-26 15:13:14 +0500

    second commit

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props ={
+    question: string
+    answer: string[]
+    callback:any
+    userAnswer:string
+    questionNr: number
+    totalQuestions:number
+}
+
+const QuestionCard=()=>(
+<div>QuestionCard</div>
+)
+
+export default QuestionCard
